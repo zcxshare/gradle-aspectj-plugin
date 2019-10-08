2bf70c5287e6ba297e760443c3972926f507258d
周超翔 <zhouchaoxiang@techb.top>
2019-10-08 18:32:39 +0800

    初始版本1.0.0

--- sample/src/main/java/com/zcx/zcxaspectj/aspect/TestAspect.java
+++ sample/src/main/java/com/zcx/zcxaspectj/aspect/TestAspect.java
@@ -4,36 +4,36 @@ import android.util.Log;
 
 import com.zcx.zcxaspectj.anno.TestA;
 
-import org.aspectj.lang.ProceedingJoinPoint;
-import org.aspectj.lang.Signature;
-import org.aspectj.lang.annotation.Around;
-import org.aspectj.lang.annotation.Aspect;
-import org.aspectj.lang.annotation.Pointcut;
-import org.aspectj.lang.reflect.MethodSignature;
+//import org.aspectj.lang.ProceedingJoinPoint;
+//import org.aspectj.lang.Signature;
+//import org.aspectj.lang.annotation.Around;
+//import org.aspectj.lang.annotation.Aspect;
+//import org.aspectj.lang.annotation.Pointcut;
+//import org.aspectj.lang.reflect.MethodSignature;
 
 /**
  * author:  zhouchaoxiang
  * date:    2019/9/30
  * explain:
  */
-@Aspect
+//@Aspect
 public class TestAspect {
     private static final String TAG = "TestAspect";
 
-    @Pointcut("@annotation(com.zcx.zcxaspectj.anno.TestA)")
+//    @Pointcut("@annotation(com.zcx.zcxaspectj.anno.TestA)")
     public void requestPermissionPointcut() {
     }
 
-    @Around("requestPermissionPointcut(needPermission)")
-    public void requestPermissionAround(final ProceedingJoinPoint joinPoint) {
-        MethodSignature signature = (MethodSignature) joinPoint.getSignature();
-        TestA testA = signature.getMethod().getAnnotation(TestA.class);
-        Log.i(TAG, "requestPermissionAround: 进入切面 value:"+testA.value());
-        try {
-            joinPoint.proceed();
-        } catch (Throwable e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-        }
-    }
+//    @Around("requestPermissionPointcut(needPermission)")
+//    public void requestPermissionAround(final ProceedingJoinPoint joinPoint) {
+//        MethodSignature signature = (MethodSignature) joinPoint.getSignature();
+//        TestA testA = signature.getMethod().getAnnotation(TestA.class);
+//        Log.i(TAG, "requestPermissionAround: 进入切面 value:"+testA.value());
+//        try {
+//            joinPoint.proceed();
+//        } catch (Throwable e) {
+//             TODO Auto-generated catch block
+//            e.printStackTrace();
+//        }
+//    }
 }
